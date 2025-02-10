39d1a514b631b15a10f19ca9d7e14b5d31cea91d
Jeon Hyejin <hyejda00@naver.com>
2025-02-11 01:01:36 +0900

    타입스크립트 기초 1.1 원시타입과 리터럴타입

---
+++ section02/src/chapter01.ts
@@ -0,0 +1,27 @@
+//number 
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "hello";
+let str2: string = 'hello';
+let str3: string = `hello`;
+let str4: string = `hello ${num1}`;
+
+// boolean 
+let boo1: boolean = true;
+let boo2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// 리터럴타입 : 변수의 타입을 값 그 자체로 정의하는 것
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
